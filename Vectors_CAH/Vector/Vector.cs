5d04aab839bc187608c5a65a5b9f9a5d133b22ac
LimESPS <72739674+LimESPS@users.noreply.github.com>
2021-04-26 11:52:54 +0100

    Vectors

---
+++ Vectors_CAH/Vector/Vector.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector_
+{
+    public class Vector
+    {
+        public List<double> v;
+        public Vector(List<double> vec)
+        {
+            v = vec;
+        }
+        public Vector add(List<Vector> vectors)
+        {
+
+            Vector result = new Vector(new List<double>());
+            for (int pos = 0; pos < vectors[0].v.Count(); ++pos)
+            {
+                double i = 0;
+                foreach (Vector item in vectors)
+                {
+                    i += item.v[pos];
+                }
+                result.v.Add(i);
+            }
+            return result;
+        }
+        public Vector scalar_multiply(double scalar)
+        {
+            Vector res = new Vector(new List<double>());
+            foreach (int item in v)
+            {
+                res.v.Add(item * scalar);
+            }
+            return res;
+        }
+        public double dot_product(List<Vector> collection)
+        {
+            Vector result = new Vector(new List<double>());
+            for (int pos = 0; pos < collection[0].v.Count(); ++pos) //same as add except i am multiplying
+            {
+                double i = 1;
+                foreach (Vector item in collection)
+                {
+                    i *= item.v[pos];
+                }
+                result.v.Add(i);
+            }
+            return result.v.Sum();
+        }
+
+        public Vector convex_combination(List<Vector> collection,double a, double b)
+        {
+            Vector first = collection[0];
+            Vector second = collection[1];
+            return first.add(new List<Vector> { first.scalar_multiply(a), second.scalar_multiply(b) });
+        }
+
+        public double Geometric_dot_product(Vector u, Vector v, double angle)
+        {
+            double res1 = u.Vector_Mag() + v.Vector_Mag();
+            double res2 = res1 * Math.Cos(angle);
+            return res2;
+        }
+        public double Vector_Mag()
+        {
+            double i = 0;
+            foreach(double item in v)
+            {
+                i += (item * item);
+            }
+            return i;
+        }
+        public double find_Angle(Vector u) //only works for vectors that form right angled trianles. will create another function
+        {
+            double adjacent = Vector_Mag();
+            double hypotoneuse = u.Vector_Mag();
+            double entry = adjacent / hypotoneuse;
+            return Math.Acos(entry);
+        }
+    }
+}
---
+++ Vectors_CAH/Vectors_CAH/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vector_;
+namespace Vectors_CAH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector u = new Vector(new List<double> { 1, 5, 3 });
+            Vector v = new Vector(new List<double> { 4, 6, 9 });
+            List<Vector> UandV = new List<Vector> { v, u };
+            Vector result_add = v.add(UandV);
+            Out(result_add);
+            Vector scalar = v.scalar_multiply(5);
+            Out(scalar);
+            Console.WriteLine(v.dot_product(UandV));
+            Out(v.convex_combination(UandV, 0.5, 0.5));
+            Console.WriteLine("Geo dot product is: "+v.Geometric_dot_product(v, u,30));
+            Console.WriteLine(u.find_Angle(v)+" radians");
+            Console.ReadKey();
+        }
+        static void Out(Vector v)
+        {
+            Console.WriteLine(string.Join(",", v.v));
+        }
+    }
+}
